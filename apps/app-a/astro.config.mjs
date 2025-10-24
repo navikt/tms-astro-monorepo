adc9f2b58f9af1cd3c8dad5d8cf619e935a00c51
Sergio A. Arevalo Soria <sergio.alberto.arevalo.soria@nav.no>
2025-10-24 17:58:39 +0200

    Fiks deploy av app-a

--- apps/app-a/astro.config.mjs
+++ apps/app-a/astro.config.mjs
@@ -1,4 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
-export default defineConfig({});
+export default defineConfig({
+    build: {
+        inlineStylesheets: "never"
+    },
+    output: "server",
+    adapter: node({
+        mode: 'standalone',
+    }),
+});
