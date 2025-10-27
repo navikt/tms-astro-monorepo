5462dae470d42dbe0b9b5c044254a97a9a77ac49
sasoria <sergio.alberto.arevalo.soria@nav.no>
2025-10-27 12:38:38 +0100

    Deploy app-b

--- apps/app-b/astro.config.mjs
+++ apps/app-b/astro.config.mjs
@@ -1,5 +1,13 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
+import node from '@astrojs/node';
 
-// https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    build: {
+        inlineStylesheets: "never"
+    },
+    output: "server",
+    adapter: node({
+        mode: 'standalone',
+    }),
+});
